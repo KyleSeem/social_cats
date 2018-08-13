9584d02485fca1fb7875f49db4a37083e79a09d6
Kyle Seem <kyle.seem@gmail.com>
2018-08-13 17:41:04 -0400

    created admin.py file

--- apps/social_media/admin.py
+++ apps/social_media/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import Profile, Post, Comment
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('display_user_id', 'user', 'nickname', 'location', 'dob', 'display_bio', 'avatar')
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'photo', 'caption', 'com_count', 'likes')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
