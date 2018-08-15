1b1f95bf4264b953bc65682b81f07d84c43d1294
Kyle Seem <kyle.seem@gmail.com>
2018-08-15 16:52:37 -0400

    fixed issue with redirect - was missing ^

--- social_cats/urls.py
+++ social_cats/urls.py
@@ -29,5 +29,5 @@ if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += {
-    url(r'', RedirectView.as_view(url='/kittenstagram/')),
+    url(r'^', RedirectView.as_view(url='/kittenstagram/')),
 }
